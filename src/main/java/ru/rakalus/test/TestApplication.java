e06913a8c7f82398e5a1599f0d5b9aae869e05dc
RaKaLuS <sir.d-i-a2014@mail.ru>
2024-02-12 23:17:21 +0700

    liquibase configuration

--- src/main/java/ru/rakalus/test/TestApplication.java
+++ src/main/java/ru/rakalus/test/TestApplication.java
@@ -2,12 +2,26 @@ package ru.rakalus.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
